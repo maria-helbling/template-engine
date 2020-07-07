59f019085966f134e4d2c54efce5739403e0df0b
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-07-07 16:37:20 -0700

    new manager instance function created

--- Develop/app.js
+++ Develop/app.js
@@ -4,16 +4,68 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const questionObj = require('./lib/querstions')
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employees = [];
+const questions = questionObj.questions
+
+//create a new Manager object
+const addManager = () => {
+    //push a Manager specific question to the questions array
+    questions.push({
+        type: 'input',
+        message: 'What is the Manager\'s office number?',
+        name: 'officeNumber'
+    })
+    //get input
+    inquirer.prompt(questions).then(answers => {
+        //new manager object
+        const newGuy = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        //push it to the employee array
+        employees.push(newGuy)
+        //start over
+        inputEmployees();
+    }).catch(err => err)
+
+}
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//Initiate user input gathering
+const inputEmployees = () => {
+    //start employee input with prompt
+    inquirer.prompt([{
+        type: 'list',
+        message: 'Pick an employee type to enter or quit.',
+        name: 'type',
+        choices: () => {
+            //if the function runs for the firs time, we don't want the option of results rendering to show
+            if (employees.length) {
+                questions.pop() //this is here to restore the questions array back to the default Employee questions
+                return ['Manager', 'Engineer', 'Intern', 'Render results']
+            } else {
+                return ['Manager', 'Engineer', 'Intern']
+            }
+        }
+    }]).then(answer => {
+        if (answer.type === 'Render results') {
+            console.log('Rendering results')
+        } else if (answer.type === 'Manager') {
+            addManager();
+        } else if (answer.type === 'Engineer') {
+            console.log('Engineer')
+            // addEngineer();
+        } else {
+            console.log('Intern')
+            // addIntern();
+        }
+    }).catch(err => err)
+}
 
+inputEmployees();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
---
+++ Develop/lib/querstions.js
@@ -0,0 +1,20 @@
+//the default employee form questions
+const questions=[
+    {
+        type: 'input',
+        message: 'What is the employee name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee id?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s email?',
+        name: 'email'
+    }
+]
+
+module.exports ={ questions}
